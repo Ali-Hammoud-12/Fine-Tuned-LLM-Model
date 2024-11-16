6951cf6807de6dda4198f96a4431e76eefcafb59
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2024-11-16 16:09:58 +0200

    Overall Code improvement

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
-from openai import OpenAI
-client = OpenAI(api_key="")
+from flask import Flask, render_template, request
+import openai 
+import config
 
-question = input("What would you like to ask ChatGPT? ")
-response = client.chat.completions.create(
-    model="",
-    messages=[{"role": "user", "content": f"{question}"}],
-    max_tokens=512,
-    n=1,
-    stop=None,
-    temperature=0.8,
-)
-# print(response)
-answer = response.choices[0].message.content
-print("OpenAI:" + answer)
+
+# __name__ is a special variable that is automatically set by Python.
+
+'''
+Its value depends on how the script is being run: 
+If the script is run directly (e.g., python script.py):
+__name__ will be set to "__main__".
+If the script is imported as a module in another script (e.g., import script):
+__name__ will be set to the name of the script (e.g., "script").
+
+'''
+openai.api_key = config.API_KEY
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_chatbot_response():
+    userText = request.args.get('msg')  # Get the user's input from the query parameter
+    if userText:
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-4o-mini",  
+                messages=[{"role": "user", "content": f"{userText}" }],
+                max_tokens=512,
+                n=1,
+                stop=None,
+                temperature=0.8,
+            )
+            answer = response['choices'][0]['message']['content']
+            return str(answer)
+        except openai.error.OpenAIError as e:
+            return f"Error: {e}"
+    else:
+        return "Error: No message received from the user"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  
+
+    
+    
