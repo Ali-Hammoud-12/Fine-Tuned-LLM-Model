b8487ed74a276cd336c0bfaba5a6b00a6f27735c
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2025-02-15 13:30:25 +0200

    Removed all OpenAI files

--- chatbot/config.py
+++ chatbot/config.py
@@ -1,12 +1,10 @@
-from chatbot.utils.train_model import train_module_with_dataset
-
-def initialize_fine_tuned_model():
-    """
-    Initializes the fine-tuned model by training it if necessary.
+# def initialize_fine_tuned_model():
+#     """
+#     Initializes the fine-tuned model by training it if necessary.
     
-    Returns:
-        str: The model ID of the fine-tuned model.
-    """
-    fine_tuned_model = train_module_with_dataset()
-    return fine_tuned_model
+#     Returns:
+#         str: The model ID of the fine-tuned model.
+#     """
+#     fine_tuned_model = train_module_with_dataset()
+#     return fine_tuned_model
 
