373ceded4d3dbff53e6fecb8fca97184060cef01
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2025-02-22 19:34:43 +0200

    Fine-Tune LLM Model Oauth2.0 done and confiured correctly

--- chatbot/app.py
+++ chatbot/app.py
@@ -3,6 +3,10 @@ import threading
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 
+import google.generativeai as genai
+
+from chatbot.utils.load_creds import load_creds
+
 from chatbot.controller.home_controller import home_bp
 from chatbot.controller.chat_controller import chat_bp
 from chatbot.controller.tuning_job_controller import tuning_bp
@@ -14,6 +18,10 @@ from chatbot.model.custom_gemini_model import CustomGemini_Model
 
 def create_app():
     load_dotenv()
+    creds = load_creds()
+    genai.configure(credentials=creds)
+    genai.configure(transport='grpc')
+    
     app = Flask(__name__)
 
     # Initialize the model singleton
