212837bb6c8a9c046662a2f0d5fcaf35d5ebe68a
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2025-04-15 21:44:50 +0300

    changed python cache sequence

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from chatbot.app import create_app  # adjust according to how your app is create
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0" , port= 5000, debug=True)
+    app.run(host="0.0.0.0" , port= 5000)
