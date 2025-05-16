3df13fd8ea748e040c91283a2b29cdbb01e93d8f
Ali-Hammoud-12 <alihammoudah12@gmail.com>
2025-05-16 21:55:09 +0300

    change in .env format

--- backend/google-service-dumper.py
+++ backend/google-service-dumper.py
@@ -2,5 +2,5 @@ import json
 import os
 
 with open(os.path.join(os.path.dirname(__file__), "client-google-services.json"), "r") as f:
-    content = f.read()
-    print(json.dumps(content)) 
+    obj = json.load(f)
+    print(json.dumps(obj))
